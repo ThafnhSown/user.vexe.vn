140372147f2d0b3fedaaccc2618057da8d1d4bab
ThafnhSown <sontnt204685@gmail.com>
2024-03-16 21:23:10 +0700

    update

--- src/screens/app/user/components/NewsCard/index.js
+++ src/screens/app/user/components/NewsCard/index.js
@@ -4,8 +4,8 @@ import news from '../../../../../assets/news.png'
 const NewsCard = ({news}) => {
     return (
         <div className='mobile:w-full desktop:w-1/4'>
-            <div className='flex flex-col'>
-                <img src={news.imageUrl}/>
+            <div className='flex flex-col "w-40 h-60 overflow-hidden"'>
+                <img className='object-cover w-full h-40' src={news.imageUrl}/>
                 <h1 className='font-bold truncate mobile:text-lg desktop:text-md'>{news.title}</h1>
                 <p className="line-clamp-4">{news.content}</p>
 
