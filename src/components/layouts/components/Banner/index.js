6f9b8f581b9a270d7e0c90fdc2f94d016b1e6d37
ThafnhSown <sontnt204685@gmail.com>
2024-03-17 20:52:48 +0700

    ss

--- src/components/layouts/components/Banner/index.js
+++ src/components/layouts/components/Banner/index.js
@@ -17,13 +17,7 @@ const Banner = () => {
         <div className='flex items-center flex-row justify-center relative'>
             <div className='flex items-center flex-col relative' style={{backgroundColor:'#f3f3f3'}}>
                 {
-                    listMainBanner.length ? <Carousel autoplay>
-                        {
-                            listMainBanner.map(banner => <div>
-                                <img src={banner.imageUrl}/>
-                                </div>)
-                        }
-                    </Carousel> : <img src={banner} className='max-w-full'/>
+                    listMainBanner.length ? <img src={listMainBanner[listMainBanner.length-1].imageUrl}/> : <img src={banner} className='max-w-full'/>
                 }
             </div>
             <FindCoach />
